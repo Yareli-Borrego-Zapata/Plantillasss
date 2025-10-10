ef430d1659838036fe1b0e2d63d704f9d80a7f8f
Yareli-Borrego-Zapata <23308060610278@cetis61.edu.mx>
2025-10-09 20:37:57 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/base")
+def menu():
+    return render_template("base.html")
+
+@app.route("/animales")
+def ani():
+    return render_template("animales.html")
+
+@app.route("/vehiculos")
+def vei():
+    return render_template("vehiculos.html")
+
+@app.route("/maravillas")
+def mun():
+    return render_template("maravillas.html")
+
+@app.route("/acerca")
+def ace():
+    return render_template("acerca.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
